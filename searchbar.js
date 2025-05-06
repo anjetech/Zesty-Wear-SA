6c31ea67e31cbc729b4598f587f2537a94746e2d
AnjeNieuwenhuis <anjenieuwenhuis@gmail.com>
2025-05-06 14:57:05 +0200

    javascript

--- searchbar.js
+++ searchbar.js
@@ -5,7 +5,6 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
 
     let pages = {
         "home": "main.html",
-
         "men": "men.html",
         "woman": "woman.html",
         "accessories": "accessories.html",
@@ -14,36 +13,27 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
         "winter jacket": "description2-men.html",
         "winter jackets": "description2-men.html",
         "jackets": "description2-men.html",
-
-
         "men's black t-shirt": "description3-men.html",
         "mens black t-shirt": "description3-men.html",
         "black t-shirt": "description3-men.html",
-
         "men's white t-shirt": "description4-men.html",
         "mens white t-shirt": "description4-men.html",
         "white t-shirt": "description4-men.html",
-
         "woman jeans": "woman-description1.html",
-
+        "jeans": "woman-description1.html",
+        "bootleg jeans": "woman-description1.html",
         "jeans suit": "woman-description2.html",
         "woman jeans suit": "woman-description2.html",
-
         "black leather jacket": "woman-description3.html",
         "woman black leather jacket": "woman-description3.html",
-
+        "leather jacket": "woman-description3.html",
         "white dress": "woman-description4.html",
         "dress": "woman-description4.html",
-
         "wallet": "accessories1-men.html",
-
         "watch": "accessories2-men.html",
-
         "belt": "accessories3-men.html",
-
         "sneakers": "accessories4-men.html",
         "sneaker": "accessories4-men.html",
-
         "earrings": "accessories1-woman.html",
         "handbag": "accessories2-woman.html",
         "make up bag": "accessories3-woman.html",
