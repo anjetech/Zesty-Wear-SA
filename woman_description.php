d12fd3b119bba3ee2ccb0ca9d5018bb073a32fff
AnjeNieuwenhuis <anjenieuwenhuis@gmail.com>
2025-06-13 15:20:34 +0200

    changed something

--- woman_description.php
+++ woman_description.php
@@ -122,7 +122,7 @@ $sizes = explode(",", $product['size']); // Splits sizes stored as "M,L,S,S" int
     <input type="hidden" name="image" value="<?php echo htmlspecialchars($product['image_path']); ?>">
     <input type="hidden" name="size" value="<?php echo htmlspecialchars(trim($sizes[0])); ?>"> <!-- Store first available size -->
 
-    <button type="submit" class ="add-to-cart">Add to Cart</button>
+    <button type="submit" name ="add-to-cart" class = "add-to-cart">Add to Cart</button>
         </div>
     </div>
   </div>
